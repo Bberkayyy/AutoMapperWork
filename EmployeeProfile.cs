cca0236e6726f7f7386032cd3324265a93025248
Bberkayyy <bberkayoguz@gmail.com>
2023-11-10 15:35:43 +0300

    Add project files.

---
+++ EmployeeProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace AutoMapperWork.Models;
+
+public class EmployeeProfile:Profile
+{
+    public EmployeeProfile()
+    {
+        CreateMap<Employee, EmployeeDTO>().ForMember(d=>d.FullName,o=>o.MapFrom(s=>s.FirstName + " " +s.LastName)).ReverseMap();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//AutoMapper => client tarafında göstermek istediğimiz alanlarla kullanmak istemedimiz alanların yeni sınıfta birleştirilmesini kolaylaştırır.
+//              client tarafında göstermek istediğimiz alanları sınırlandırarak gerçek nesneyi korumayı sağlar.
